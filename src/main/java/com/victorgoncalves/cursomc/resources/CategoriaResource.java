5ea13fe0d98eec19b97abf759a4aaa9493ee1750
Victor Silva <victorgoncalves.programacao@gmail.com>
2019-08-07 21:32:20 -0300

    Criando operação deinstanciacao

--- src/main/java/com/victorgoncalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/victorgoncalves/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.victorgoncalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +16,7 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService categoriaService;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria categoria = categoriaService.buscar(id);
